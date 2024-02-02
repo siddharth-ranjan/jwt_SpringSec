18202a5ec0683d00ba954c09faf9a76c6ea0443e
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2024-02-02 21:30:08 +0530

    Initial Commit

---
+++ src/main/java/com/learn/springsec/jwt_springsec/JwtSpringSecApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springsec.jwt_springsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringSecApplication.class, args);
+    }
+
+}
